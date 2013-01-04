c97a5458a9fd57fb53081b599db543f0b9467514
Kiran Kulkarni <kk.questworld@gmail.com>
2013-01-04 18:29:17 +0530

    FIX: use web-template as renderer

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.kiran/lein-web-template "0.1.0"
+(defproject org.clojars.kiran/lein-web-template "0.2.0"
   :description "A template for generating a web-app which supports hiccup and stencil rendering"
   :url "https://github.com/kirankulkarni/lein-web-template"
   :license {:name "Eclipse Public License"
--- src/leiningen/new/web_template.clj
+++ src/leiningen/new/web_template.clj
@@ -1,9 +1,10 @@
 (ns leiningen.new.web-template
   (:use [leiningen.new.templates :only [renderer name-to-path sanitize-ns ->files]]))
 
-(def render (renderer "lein-web-template"))
+(def render (renderer "web-template"))
 
-(defn lein-web-template
+(defn web-template
+  "A skeleton Compojure-Ring project"
   [name]
   (let [data {:name name
               :ns-name (sanitize-ns name)
